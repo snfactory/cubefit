implement psf_roll; fix psf size calculation

ddtpy/core.py
@@ -49,12 +49,22 @@ class DDT(object):
         # GS-PSF --> ES-PSF
         # G-PSF --> GR-PSF
         if conf["PARAM_PSF_TYPE"] == "GS-PSF":
+            self.psf_nx, self.psf_ny = 32, 32
+            offx = int((self.psf_nx-1) / 2.)
+            offy = int((self.psf_ny-1) / 2.)
+
+            # x, y coordinates on psf array (1-d)
+            self.psf_x = np.arange(-offx, self.psf_nx - offx)
+            self.psf_y = np.arange(-offy, self.psf_ny - offy)
+
+            # sn is "by definition" at array position where coordinates = (0,0)
+            self.model_sn_x = offx
+            self.model_sn_y = offy
+
             es_psf = np.array(conf["PARAM_PSF_ES"])
             ellipticity, alpha = params_from_gs(es_psf, self.wave)
-            tmp = gaussian_plus_moffat_psf_4d(32, 32, ellipticity, alpha)
-            self.psf_x, self.psf_y, self.psf = tmp
-            self.psf_nx = len(self.psf_x)
-            self.psf_ny = len(self.psf_y)
+            self.psf = gaussian_plus_moffat_psf_4d(self.psf_x, self.psf_y,
+                                                   ellipticity, alpha)
         elif conf["PARAM_PSF_TYPE"] == "G-PSF":
             raise RuntimeError("G-PSF (from FITS files) not implemented")
         else:
@@ -65,8 +75,6 @@ class DDT(object):
         self.model_galprior = np.zeros((self.nw, self.psf_ny, self.psf_nx))
         self.model_sky = np.zeros((self.nt, self.nw))
         self.model_sn = np.zeros((self.nt, self.nw))
-        self.model_sn_x = (self.psf_nx + 1) // 2
-        self.model_sn_y = (self.psf_ny + 1) // 2
         self.model_eta = np.ones(self.nt)
         self.model_final_ref_sky = np.zeros(self.nw)
 
@@ -122,6 +130,21 @@ class DDT(object):
         self.range_x = slice(offset_x, offset_x + self.data_nx)
         self.range_y = slice(offset_y, offset_y + self.data_ny)
 
+        # equilvalent of ddt_setup_apodizer() in Yorick,
+        # with without implementing all of it:
+        if self.flag_apodizer < 2:
+            self.apodizer = None
+            self.psf_enlarge = None
+        else:
+            raise RuntimeError("FLAG_APODIZER >= 2 not implemented")
+
+        # This moves the center of the PSF from array coordinates
+        # (model_sn_x, model_sn_y) -> (0, 0) [lower left pixel]
+        # I don't know why this is done.
+        self.psf = roll_psf(self.psf, -self.model_sn_x, -self.model_sn_y)
+        self.psf_rolled = True
+
+
     def __str__(self):
         """Create a string representation.
 
@@ -143,8 +166,7 @@ class DDT(object):
         """Returns just the section of the model (x) that overlaps the data.
 
         This is the same as the Yorick ddt.R operator with job = 0.
-        
-        In Yorick x could be 2 or more dimensions, here it must be 4-d.
+        In Yorick, `x` could be 2 or more dimensions; here it must be 4-d.
         """
         return x[:, :, self.range_y, self.range_x]
 

ddtpy/psf.py
@@ -38,20 +38,21 @@ def params_from_gs(es_psf, wave):
 
     return ellipticity, alpha
 
-def gaussian_plus_moffat_psf_4d(nx, ny, ellipticity, alpha, angle=None):
+def gaussian_plus_moffat_psf_4d(x, y, ellipticity, alpha, angle=None):
     """Create a 2-d PSF for a 2-d array of parameters.
 
     Parameters
     ----------
-    nx, ny : int
-        Size of output 2-d PSFs.
+    x, y : 1-d array of int
+        Coordinates of each array element in x and y. The PSF is centered
+        at coordinates (x,y) = (0.,0.).
     ellipticity : 2-d array
     alpha : 2-d array
 
     Returns
     -------
-    x : 1-d array
-    y : 1-d array
+    x : 1-d array of int
+    y : 1-d array of int
     psf : 4-d array
 
     Notes
@@ -63,7 +64,10 @@ def gaussian_plus_moffat_psf_4d(nx, ny, ellipticity, alpha, angle=None):
     """
 
     assert ellipticity.shape == alpha.shape
+
     nt, nw = ellipticity.shape
+    ny = len(y)
+    nx = len(x)
 
     if angle is None:
         angle = np.zeros(nt)
@@ -71,20 +75,15 @@ def gaussian_plus_moffat_psf_4d(nx, ny, ellipticity, alpha, angle=None):
     # allocate output array
     psf = np.empty((nt, nw, ny, nx), dtype=np.float)
 
-    # coordinates of center of each spaxel, relative to central spaxel
-    x = np.arange(1-nx//2, 1+nx//2)
-    y = np.arange(1-ny//2, 1+ny//2)
-
     for i_t in range(nt):
-        angle_t = angle[i_t]
         for i_w in range(nw):
             e = ellipticity[i_t, i_w]
             a = alpha[i_t, i_w]
-            slicepsf = gaussian_plus_moffat_psf(x, y, 0.0, 0.0, e, a,
-                                                angle=angle_t)
+            slicepsf = gaussian_plus_moffat_psf(x, y, 0., 0., e, a,
+                                                angle=angle[i_t])
             psf[i_t, i_w, :, :] = slicepsf / np.sum(slicepsf)
 
-    return x, y, psf
+    return psf
 
 def gaussian_plus_moffat_psf(x, y, x0, y0, ellipticity, alpha, angle=0.):
     """Evaluate a gaussian+moffat function on a 2-d grid. 
@@ -142,3 +141,32 @@ def gaussian_plus_moffat_psf(x, y, x0, y0, ellipticity, alpha, angle=0.):
     norm_psf = 1. / (1./norm_moffat + eta * 1./norm_gauss)
 
     return norm_psf * (moffat + eta*gauss)
+
+def roll_psf(psf, dx, dy):
+    """Rolls the psf by dx, dy in x and y coordinates.
+
+    The equivalent of the Yorick version with job = 0 is
+
+    dx, dy = (1 - sn_x, 1 - sn_y) [if sn_x = (nx+1)//2]
+    or
+    dx, dy = (-sn_x, -sn_y) [if sn_x = (nx-1)//2]  # python indexed
+
+    job=1 is 
+
+    Parameters
+    ----------
+    psf : 4-d array
+    dx, dy : int
+        Shift in x and y coordinates.
+
+    Returns
+    -------
+    rolled_psf : 4-d array
+
+    """
+
+    assert psf.ndim == 4  # just checkin' (in Yorick this is also used for 3-d)
+                          # but we haven't implemented that here.
+
+    tmp = np.roll(psf, dy, 2)  # roll along y (axis = 2)
+    return np.roll(tmp, dx, 3)  # roll along x (axis = 3)