d0f9be81b84528cb797b8f53643587fccc1e4698
Kyle Barbary <kylebarbary@gmail.com>
2014-11-04 14:11:33 -0800

    somewhat working plotting function.

--- ddtpy/plotting.py
+++ ddtpy/plotting.py
@@ -1,10 +1,14 @@
 from __future__ import print_function, division
 
+import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import NullLocator
 
-bandlimits = {'B': (
+BAND_LIMITS = {'U': (3400., 3900.),
+               'B': (4102., 5100.),
+               'V': (6289., 7607.)}
 
-def integrate_tophat(data, wmin, wmax):
+STAMP_SIZE = 1.5
 
 def plot_timeseries(data, model=None, band='B'):
     """Return a figure showing data and model.
@@ -16,20 +20,41 @@ def plot_timeseries(data, model=None, band='B'):
     band : str
     """
 
-    STAMP_SIZE = 1.5
-
     # one column for each data epoch, plus 2 extras for model
     ncol = data.nt + 2
     nrow = 4
     figsize = (STAMP_SIZE * ncol, STAMP_SIZE * nrow)
+    fig = plt.figure(figsize=figsize)
 
-    # Create figure and all the axes:
-    fig, axes = plt.subplots(nrow, ncol, figsize=figsize, squeeze=False)
+    # upper and lower wavelength limits
+    wmin, wmax = BAND_LIMITS[band]
+
+    # plot model, if given
+    if model is not None:
+        ax = plt.subplot2grid((nrow, ncol), (0, 0), rowspan=2, colspan=2)
+        mask = (model.wave > wmin) & (model.wave < wmax)
+        image = np.average(model.gal[mask, :, :], axis=0)
+        ax.imshow(image, vmin=image.min(), vmax=image.max(), cmap='Greys',
+                  interpolation='nearest', origin='lower')
+        ax.xaxis.set_major_locator(NullLocator())
+        ax.yaxis.set_major_locator(NullLocator())
+
+    # compute all images ahead of time so that we can set vmin, vmax
+    # the same for all.
+    wmin, wmax = BAND_LIMITS[band]
+    mask = (data.wave > wmin) & (data.wave < wmax)
+    images = np.average(data.data[:, mask, :, :], axis=1)
+    vmin = images.min()
+    vmax = images.max()
+
+    for i_t in range(data.nt):
+        ax = plt.subplot2grid((nrow, ncol), (0, i_t + 2))
+        ax.imshow(images[i_t], vmin=vmin, vmax=vmax, cmap='Greys',
+                  interpolation='nearest', origin='lower')
+        ax.xaxis.set_major_locator(NullLocator())
+        ax.yaxis.set_major_locator(NullLocator())
+
     fig.subplots_adjust(left=0.001, right=0.999, bottom=0.02, top=0.98,
                         hspace=0.01, wspace=0.01)
 
-    for i_t in range(data.nt):
-        ax = axes[0, i_t]
-
-        
-        ax.imshow(data.data[i_t]
+    return fig
