1ea80ac7538e1091dbdfca10948cbb566e479a44
Kyle Barbary <kylebarbary@gmail.com>
2015-07-07 21:25:39 -0700

    change default mu parameters back to original

--- cubefit/main.py
+++ cubefit/main.py
@@ -126,7 +126,7 @@ def setup_logging(loglevel, logfname=None):
 
 def main(configfname, outfname, dataprefix="", logfname=None,
          loglevel=logging.INFO, diagdir=None, refitgal=False,
-         mu_wave=5.e-4, mu_xy=1.e-5, **kwargs):
+         mu_wave=0.07, mu_xy=0.001, **kwargs):
     """Run cubefit.
 
     Parameters
